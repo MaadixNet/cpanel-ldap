1a6044d5475cf5973ce0378d6f8b8ef19e3265a6
root <root@ip16.maadix.org>
2016-12-06 23:26:52 +0100

    Version 0.1.2.2 Alpha

--- site-config-example.php
+++ site-config-example.php
@@ -13,21 +13,68 @@ define ("LDAP_TLS",0);
 // The container
 define ("SUFFIX","dc=example,dc=tld");
 
-// The admin bind dn (could be rootdn)
-define ("BINDDN","cn=admin,dc=example,dc=tld");
+// The admin bind dn 
+# Get value from function. if it doesn't work set it manually
+$binddn =  get_bind_dn();
+define ("BINDDN",$binddn);
 
 // The Phamm container - change it if your installation has different structure
-define ("LDAP_BASE","o=hosting,dc=example,dc=tld");
+define ("LDAP_BASE","o=hosting," . SUFFIX);
 
-//The VPN container
-define ("LDAP_VPN","ou=vpn,dc=example,dc=tld");
+//The People container for sftp users and vpn users
+define ("LDAP_PEOPLE","ou=sshd,ou=People," .  SUFFIX);
 
-//reader user for ldapserch.sh script. If you don't have any only read user you can use admin,
-////but for security it would be better to have a lower privilege user with no write permissions
-////we can use the default phamm user
-define ("READDN","cn=phamm,dc=example,dc=tld");
-
-////read user password to allow ldapserch.sh to bind ldap
-define ("RDNPSW","rhx");
 //To create internal links
 define ("BASE_PATH" , basename(__DIR__));  
+
+// The languages available
+ $supported_languages = array();
+// $supported_languages["de_DE"] = "Deutsch";
+ $supported_languages["en_GB"] = "English";
+ $supported_languages["es_ES"] = "Español";
+// $supported_languages["fr_FR"] = "French";
+// $supported_languages["hu_HU"] = "Hungarian";
+// $supported_languages["it_IT"] = "Italiano";
+// $supported_languages["pl_PL"] = "Polish";
+// $supported_languages["ru_RU"] = "Russian";
+// $supported_languages["vi_VN"] = "Tiếng Việt"; // Vietnamese
+// $supported_languages["da_DK"] = "Dansk"; // Danish
+// $supported_languages["pt_BR"] = "Portuguese";
+
+
+ # Get admin name dinamically throug a ESXTERNAL bind connection
+ # Ldap need to be configured in oreder to allow 
+ # anonymous bind connection for apache
+ # If you don't want this kind od configuratios for your ldaps
+ # server define BINDDN manually...as eg: cn=admin,dc=example,dc=tld 
+
+## Get the admin bame
+    function get_bind_dn () {
+      //Special connection Only read mode to rertieve cn and mail
+      $host = "ldapi:///";
+      $base = SUFFIX;
+
+      $ds = ldap_connect($host);
+
+      //buscamos cualquier entrada
+      $filter="(&(objectclass=extensibleObject)(!(cn=uidNext)))";
+      //de las entradas solo queremos cn y mail
+      $justthese = array("cn");
+
+      //como usuario anonimo solo tenemos acceso al primer nivel de la base de
+      //datos, asi que solo tenemos acceso al dn de admin. y solo tenemos acceso
+      //a su atributo cn e email.
+
+      $sr=ldap_search($ds, $base, $filter, $justthese);
+      $info = ldap_get_entries($ds, $sr);
+      /*echo $info["count"]." entradas devueltas\n";
+      echo "<pre>";
+      print_r ($info);
+      echo "</pre>";
+      */
+      $adminname = $info[0]["dn"];
+      return $adminname;
+      }  
+
+
+
