77547a4ab6465d1423adb207ddbf4aa5318c5fc9
root <root@bang.maadix.org>
2016-12-30 12:54:11 +0100

    error reporting disabled

--- edit-user.php
+++ edit-user.php
@@ -1,6 +1,6 @@
 <?php 
-error_reporting(-1);
-ini_set('display_errors', 'On');
+//error_reporting(-1);
+//ini_set('display_errors', 'On');
 
 session_start();
 require_once 'classes/class.ldap.php';
