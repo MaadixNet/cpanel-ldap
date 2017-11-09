71b7dd792ef548c7cb6896fc155c985686ab2382
Klunt <32511491+Klunt@users.noreply.github.com>
2017-11-09 12:02:11 +0100

    Update processing.php

--- processing.php
+++ processing.php
@@ -10,7 +10,7 @@ require_once('header.php');?>
 <?php
 echo '<h3>'. sprintf(_("Procesando operación")). '</h3>';
 echo '<p>'. sprintf(_("Todos los datos para recuperar la contraseña han sido insertados correctamente.")). '</p>';
-echo '<p>'. sprintf(_("Recibirás un mail de confirmación en cuanto la contraseña haya sido restablecida.")). '</p>';
+echo '<p>'. sprintf(_("Recibirás un correo de confirmación en cuanto la contraseña haya sido restablecida.")). '</p>';
 echo '<p>'. sprintf(_("Este proceso puede tardar hasta 10 minutos.")). '</p>';
 ?>
 </div><!--admin-content-->
