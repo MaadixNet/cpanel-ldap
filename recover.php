739149837b24cefd84d2a0e9436a0c3810b0f9d3
MaadixNet <admin@maadix.net>
2018-03-11 21:24:03 +0100

    Updated title in recover page

--- recover.php
+++ recover.php
@@ -19,7 +19,7 @@ if($user_home->is_logged_in())
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title> ModularAdmin - Free Dashboard Theme | HTML Version </title>
+        <title> Recover | Maadix </title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
