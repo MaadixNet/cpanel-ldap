8529383c38a675b5b0fd3a70c1061dbf7a39d0df
root <root@usavm2.gadix.net>
2018-06-24 00:20:28 +0200

    custom status js for fqdn change login

---
+++ js/fetch-status.js
@@ -0,0 +1,54 @@
+$( ".form-signin" ).hide();
+$( ".updating" ).hide();
+var thenewUrl='';
+if (document.getElementById("newUrl")) {
+
+  var div= document.getElementById("newUrl");
+  thenewUrl = div.getAttribute("data-url");
+  console.log(thenewUrl);
+}
+var theUrl = "/cpanel/status.php";
+
+function httpGetAsync(theUrl)
+
+{
+
+fetch(theUrl) // Call the fetch function passing the url of the API as a parameter
+.then(function(response) {
+    console.log(response);
+    // Your code for handling the data you get from the API
+  if(response.status == 200){
+      return response.text();
+  }
+  })
+  .then(function(text) {
+    console.log('Request successful', text);
+          if(text=='ready') {
+             //UNLOCK GUI
+              //console.log('ready');
+              $( ".form-signin" ).show();
+              $( ".updating" ).hide();
+              clearInterval(httpGetAsync);
+
+            }else{
+              //LOCK GUI
+              //console.log('locked');
+              $( ".form-signin" ).hide();
+              $( ".updating" ).show();
+               setTimeout(function() {
+                     httpGetAsync(theUrl);  
+               }, 2000);
+                 
+            }
+  })
+
+.catch(function(error) {
+   clearInterval(httpGetAsync);
+   console.log('Fetch Error:', error);
+   setTimeout(function() {
+        $( ".form-signin" ).show();
+        $( ".updating" ).hide();
+   }, 10000);
+});
+}
+httpGetAsync(theUrl);
