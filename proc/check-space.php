d8f580f7156782cca039038c0a6e79d7d226901c
root <root@usavm2.maadix.org>
2018-06-26 10:14:05 +0200

    img size

--- proc/check-space.php
+++ proc/check-space.php
@@ -2,11 +2,11 @@
 /**
  * Created by Maddish
  *
- * function file called by service-available.php
- * Check required fields of groups
+ * 
+ * Check space on disk
  *
  */
-session_start();
+//session_start();
 require_once __DIR__.'/../classes/class.DiskStatus.php';
 try {
   $diskStatus = new DiskStatus('/');
